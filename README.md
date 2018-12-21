abf9ac0e4b98a145ba16f1da26a331f88e623c35
Alejandra Giraldo <m.aleja3000@gmail.com>
2018-12-20 22:23:56 -0500

    Add Creation Of Personal Card

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
-It's me, Tierney!
+Hi! I'm Maleja
+And this is the [repository](https://github.com/bnb/bitandbang) based. By [Tierney](https://twitter.com/bitandbang)
 
 # Usage
 Via npx:
 ```
-npx bitandbang
+npx maleja
 ```
 
 Not including how to use it globally because I'm not sure why you'd want this as a global command. That'd be creepy.
--- bin/card.js
+++ bin/card.js
@@ -14,33 +14,45 @@ const options = {
 
 // Text + chalk definitions
 const data = {
-  name: chalk.white('Tierney Cyren /'),
-  handle: chalk.cyan('bitandbang'),
-  work: chalk.white('Senior Cloud Developer Advocate at Microsoft'),
-  twitter: chalk.cyan('https://twitter.com/bitandbang'),
-  github: chalk.cyan('https://github.com/bnb'),
-  linkedin: chalk.cyan('https://linkedin.com/in/bitandbang'),
-  web: chalk.cyan('https://bnb.im'),
-  npx: chalk.white('npx bitandbang'),
+  boxTop: chalk.magenta('◤★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★◥'),
+  boxSide: chalk.magenta('★'),
+  name: chalk.cyan.bold('  Alejandra Giraldo /'),
+  bio: chalk.white.bold('       ❣ In love with Coding ❣       '),
+  handle: chalk.cyan('maleja on npm  '),
+  boxBottom: chalk.magenta('◣★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★◢'),
+  workFront: chalk.white('Frontend With Angular'),
+  workBack: chalk.white('Backend With Node.js'),
+  logoNode: chalk.green('⬢'),
+  logoAngular: chalk.bgRed('A'),
+  twitter: chalk.cyan('https://twitter.com/maleja111'),
+  github: chalk.cyan('https://github.com/maleja111'),
+  linkedin: chalk.cyan('https://linkedin.com/in/maleja1111'),
+  npx: chalk.white('npx maleja'),
   labelWork: chalk.white.bold('      Work:'),
   labelTwitter: chalk.white.bold('   Twitter:'),
   labelGitHub: chalk.white.bold('    GitHub:'),
   labelLinkedIn: chalk.white.bold('  LinkedIn:'),
-  labelWeb: chalk.white.bold('       Web:'),
   labelCard: chalk.white.bold('      Card:')
 }
 
 // Actual strings we're going to output
 const newline = '\n'
+const space = '              '
+const boxTop = `${data.boxTop}`
 const heading = `${data.name} ${data.handle}`
-const working = `${data.labelWork}  ${data.work}`
+const bio = `${data.bio}`
+const boxSide = `${data.boxSide}`
+const boxBottom = `${data.boxBottom}`
+const working = `${data.labelWork}  ${data.workFront} ${data.logoAngular} ${data.workBack} ${data.logoNode}`
 const twittering = `${data.labelTwitter}  ${data.twitter}`
 const githubing = `${data.labelGitHub}  ${data.github}`
 const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
-const webing = `${data.labelWeb}  ${data.web}`
 const carding = `${data.labelCard}  ${data.npx}`
 
 // Put all our output together into a single variable so we can use boxen effectively
-const output = heading + newline + newline + working + newline + twittering + newline + githubing + newline + linkedining + newline + webing + newline + newline + carding
+const output = space + boxTop + newline + space + boxSide + heading + boxSide
+  + newline + space + boxSide + bio + boxSide + newline + space + boxBottom
+  + newline.repeat(2) + newline + working + space + newline + twittering + newline + githubing
+  + newline + linkedining + newline + newline + carding
 
 console.log(chalk.green(boxen(output, options)))
